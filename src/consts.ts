0e2dee5f8b42521b3b353a4e4c9744a2e1ce44b0
daanjangrotenhuis <daan.jan.grotenhuis@bookingbooster.nl>
2026-05-18 15:36:17 +0200

    html fix

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Vakantie voor Kids';
-export const SITE_DESCRIPTION = 'De leukste vakanties, uitjes en reistips voor het hele gezin. Ontdek bestemmingen, vakantieparken en dagtrips die kinderen geweldig vinden.';
+export const SITE_DESCRIPTION = 'Op zoek naar de perfecte vakantie met kinderen? Blogs, tips en reviews over vakantieparken, campings en dagtrips.';
